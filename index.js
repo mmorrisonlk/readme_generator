a901cd1e89862e6a758ee154ffb3e5b01de292e0
mmorrisonlk <mmorrisonlk@gmail.com>
2021-04-07 14:36:56 -0500

    Fixed some wrong starting points and laid out basic code

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
 // Section to Input title of project Needs to be the title of the Readme
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What would you like to title this read.ME?',
+        name: 'projectTitle',
+    }
+  ])
+  .then((data) => {
+    const fileName = `${data.projectTitle.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) => err ? console.log(err) : console.log('Success!')
+    );
+  })
+//   .catch(error => {
+//     if(error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 //Description of project goes here (Needs to be a Header in the ReadME)
 //Optional Table of Contents (Links between the sections)
 //Installation? (Needs to be a Header in the ReadME)
