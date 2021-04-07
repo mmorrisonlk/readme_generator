e9e00fbccad981b94497727b50466912a6133909
mmorrisonlk <mmorrisonlk@gmail.com>
2021-04-07 14:12:09 -0500

    Commented out the requirements to organize

---
+++ index.js
@@ -0,0 +1,9 @@
+// Section to Input title of project Needs to be the title of the Readme
+//Description of project goes here (Needs to be a Header in the ReadME)
+//Optional Table of Contents (Links between the sections)
+//Installation? (Needs to be a Header in the ReadME)
+//Usage (Needs to be a Header in the ReadME)
+//License (Liscense choose from a list) Adds a badge near the top of the readme that explains what license its unter
+//Contributing (Needs to be a Header in the ReadME)
+//Tests (Needs to be a Header in the ReadME)
+//Submit Questions (Adds Github account link and Email to submit questions for)
