aada99c11024feb4e6442cd3bf4d1be0d810ebf4
Mark Morrison <markmorrison@marks-mbp.attlocal.net>
2021-04-15 19:05:47 -0500

    Added most of the prompts

--- index.js
+++ index.js
@@ -13,7 +13,38 @@ inquirer
         type: 'input',
         message: 'In a few words, describe this project',
         name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Does your project require installing dependicies?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Any usage restrictions for your project?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Any guidelines for contributing to your project?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What tests will you be using?',
+        name: 'testingInfo',
+    },
+    {
+        type: 'input',
+        message: 'What is your github account?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What email can you be reached at?',
+        name: 'emailContact',
     }
+
   ])
   .then((data) => {
     const fileName = `${data.projectTitle.toLowerCase().split(' ').join('')}.json`;
